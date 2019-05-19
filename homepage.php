09b9a0b8a751ce1e764eb2ad9049ffa155308814
998244353@sjtu.edu.cn <998244353@sjtu.edu.cn>
2019-05-19 17:27:01 +0800

    complete input.php

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="uft-8"/>
+    <frameset cols="30%,70%">
+    <frame id="leftframe" src="input.php" />
+        <frame id="rightframe" src="output.php" />
+    </frameset>
+</head>
+
+<body>
+
+</body>
+
+
+</html>
